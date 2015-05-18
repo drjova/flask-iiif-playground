371f1bec764b07fc4ffda5132857fa2cc73cbe3b
Harris Tzovanakis <me@drjova.com>
2015-05-18 16:18:49 +0200

    more nice changes

--- iiif/base/heroku.py
+++ iiif/base/heroku.py
@@ -1,3 +1,11 @@
+"""The Heroku setup."""
+
+import os
+
 from .factory import create_app
 
-app = create_app("./../instance", env="Heroku")
+instance_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), 'instance'
+)
+
+app = create_app(instance_path=instance_path, env="Heroku")
